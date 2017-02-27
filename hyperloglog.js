2aa8daa80b3eb7936016c910fcb74d4ab9a8d60b
Fabio Oliveira <fabio.an.oliveira@gmail.com>
2017-02-27 00:26:29 +0000

    some cleaning on algorithm and tests

--- hyperloglog.js
+++ hyperloglog.js
@@ -1,124 +1,106 @@
-function compute_alpha_times_bucket_count_squared(bucket_count) {
-    return 0.7213 / (1 + 1.079 / bucket_count) * bucket_count * bucket_count;
+class HyperLogLog {
+  constructor(size) {
+    this.size = size;
+    this.bucketCount = 2 ** size;
+    this.buckets = new Buffer.allocUnsafe(this.bucketCount).fill(0);
+    this.harmonicMean = harmonicMean(this.bucketCount);
+    this.sumOfInverses = this.bucketCount;
+    this.countZeroBuckets = this.bucketCount;
+  }
+
+  add(hash) {
+    if (!hash) {
+      return;
+    }
+
+    let bucket = hash[0] >>> (32 - this.size);
+    let trailingZeros = 1;
+  countZeros:
+    for (let i = 3; i >= 2; --i) {
+      let data = hash[i];
+      for (let j = 32; j; --j) {
+        if (data & 0x1) {
+          break countZeros;
+        }
+
+        trailingZeros += 1;
+        data = data >>> 1;
+      }
+    }
+
+    let oldValue = this.buckets[bucket];
+    let newValue = Math.max(trailingZeros, oldValue);
+
+    this.sumOfInverses += ((2 ** -newValue) - (2 ** -oldValue));
+    if (newValue !== 0 && oldValue === 0) {
+      this.countZeroBuckets -= 1;
+    }
+
+    this.buckets[bucket] = newValue;
+
+    return this;
+  }
+
+  count() {
+    let estimate = this.harmonicMean / this.sumOfInverses;
+
+    if (this.countZeroBuckets > 0 && estimate < 5/2 * this.bucketCount) {
+      estimate = this.bucketCount * Math.log(this.bucketCount / this.countZeroBuckets);
+    }
+
+    return Math.floor(estimate + 0.5);
+  }
+
+  relativeError() {
+    return 1.04 / Math.sqrt(this.bucketCount);
+  }
+
+  merge(data) {
+    if (this.size > data.size) {
+      let newBucketCount = 2 ** data.size;
+      let oldBucketsPerNew = 2 ** (this.size - data.size);
+      let newBuckets = new Buffer.allocUnsafe(newBucketCount).fill(0);
+
+      for (let i = 0; i < newBucketCount; ++i) {
+        let newBucketValue = data.buckets[i];
+        for (let j = 0; j < oldBucketsPerNew; ++j) {
+          newBucketValue = Math.max(newBucketValue, this.buckets[i * oldBucketsPerNew + j]);
+        }
+        newBuckets[i] = newBucketValue;
+      }
+
+      this.buckets = newBuckets;
+      this.size = data.size;
+      this.bucketCount = 2 ** this.size;
+      this.harmonicMean = harmonicMean(this.bucketCount);
+    } else {
+      let newBucketsPerExisting = 2 ** (data.size - this.size);
+
+      for (let i = data.buckets.length - 1; i >= 0; --i) {
+        let existingBucketIndex = (i / newBucketsPerExisting) | 0;
+        this.buckets[existingBucketIndex] = Math.max(this.buckets[existingBucketIndex], data.buckets[i]);
+      }
+    }
+
+    this.sumOfInverses = 0;
+    this.countZeroBuckets = 0;
+
+    for (let i = 0; i < this.bucketCount; ++i) {
+      let bucket = this.buckets[i];
+
+      if (bucket === 0) {
+        this.countZeroBuckets += 1;
+      }
+
+      this.sumOfInverses += (2 ** -bucket);
+    }
+  }
 }
 
-// Create a HyperLogLog counter of 2^n buckets.
-// 2^0 to 2^32 - requires that many BYTES (really 6 bit words for 64 bit hashing)
-// The limit of 2^32 comes from using the first 32 bit int of the hash
-// for the bucket index. Theoretically we could scale that to allow more, but that means
-// more than 4GB per HLL, which is unlikely.
-function HyperLogLog(n) {
-    var bucket_count = Math.pow(2, n);
-    var alpha_times_bucket_count_squared = compute_alpha_times_bucket_count_squared(bucket_count);
-    var buckets = new Buffer.allocUnsafe(bucket_count).fill(0);
+function harmonicMean(bucketCount) {
+  return 0.7213 / (1 + 1.079 / bucketCount) * bucketCount * bucketCount;
+}
 
-    // Maintain some running counts so that returning cardinality is cheap.
-
-    var sum_of_inverses = bucket_count;
-    var count_zero_buckets = bucket_count;
-
-    var self = {
-        add: function add(unique_hash) {
-            if (unique_hash === null) {
-                return; // nothing to add
-            }
-
-            var bucket = unique_hash[0] >>> (32 - n);
-            var trailing_zeroes = 1;
-
-            count_zeroes:
-                for (var i = 3; i >= 2; --i) {
-                    var data = unique_hash[i];
-                    for (var j = 32; j; --j) {
-                        if (data & 0x1) {
-                            break count_zeroes;
-                        }
-
-                        ++trailing_zeroes;
-                        data = data >>> 1;
-                    }
-                }
-
-            // Maintain a running sum of inverses for quick cardinality checking.
-            var old_value = buckets[bucket];
-            var new_value = Math.max(trailing_zeroes, old_value);
-            sum_of_inverses += Math.pow(2, -new_value) - Math.pow(2, -old_value);
-            if (new_value !== 0 && old_value === 0) {
-                --count_zero_buckets;
-            }
-
-            buckets[bucket] = new_value;
-
-            return self;
-        },
-
-        count: function count() {
-            var estimate = alpha_times_bucket_count_squared / sum_of_inverses;
-
-            // Apply small cardinality correction
-            if (count_zero_buckets > 0 && estimate < 5/2 * bucket_count) {
-                estimate = bucket_count * Math.log(bucket_count / count_zero_buckets);
-            }
-
-            return Math.floor(estimate + 0.5);
-        },
-
-        relative_error: function relative_error() {
-            // Estimate the relative error for this HLL.
-            return 1.04 / Math.sqrt(bucket_count);
-        },
-
-        output: function output() {
-            return {
-                n: n,
-                buckets: buckets
-            }
-        },
-
-        merge: function merge(data) {
-            if (n > data.n) {
-                // Fold this HLL down to the size of the incoming one.
-                var new_bucket_count = Math.pow(2, data.n);
-                var old_buckets_per_new_bucket = Math.pow(2, n - data.n);
-                var new_buckets = new Buffer(new_bucket_count);
-
-                for (var i = 0; i < new_bucket_count; ++i) {
-                    var new_bucket_value = data.buckets[i];
-                    for (var j = 0; j < old_buckets_per_new_bucket; ++j) {
-                        new_bucket_value = Math.max(new_bucket_value, buckets[i * old_buckets_per_new_bucket + j]);
-                    }
-                    new_buckets[i] = new_bucket_value;
-                }
-
-                buckets = new_buckets;
-                n = data.n;
-
-                bucket_count = Math.pow(2, n);
-                alpha_times_bucket_count_squared = compute_alpha_times_bucket_count_squared(bucket_count);
-            } else {
-                var new_buckets_per_existing = Math.pow(2, data.n - n);
-                for (var i = data.buckets.length - 1; i >= 0; --i) {
-                    var existing_bucket_index = (i / new_buckets_per_existing) | 0;
-                    buckets[existing_bucket_index] = Math.max(buckets[existing_bucket_index], data.buckets[i]);
-                }
-            }
-
-            // Recompute running totals
-            sum_of_inverses = 0;
-            count_zero_buckets = 0;
-            for (var i = 0; i < bucket_count; ++i) {
-                var bucket = buckets[i];
-                if (bucket === 0) {
-                    ++count_zero_buckets;
-                }
-                sum_of_inverses += Math.pow(2, -bucket);
-            }
-        }
-    };
-
-    return self;
-};
-
-module.exports = HyperLogLog;
+module.exports = exports = HyperLogLog;
 
 module.exports.hash = require('murmurhash3').murmur128Sync;
